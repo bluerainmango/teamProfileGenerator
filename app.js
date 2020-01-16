40800fcd63f4bd8192974198f888bb00f660300b
Emily Yu <bluerainmango@gmail.com>
2020-01-15 20:02:43 -0800

    revised Classes that passed all Jest tests

--- app.js
+++ app.js
@@ -2,17 +2,16 @@
 
 // prompt for team members - engineer / inern
 // name, email, id, manager: officeNumber engineer: github username, intern: school
-const {
-  askAbout,
-  managerQuest,
-  memberQuest,
-  memberCheck
-} = require("./lib/inquiry");
+const { askAbout, managerQuest, memberQuest, memberCheck } = require("./lib/inquiry");
+
+const { Manager } = require("./lib/Manager");
+const { Engineer } = require("./lib/Engineer");
+const { Inter } = require("./lib/Intern");
 
 const team = {
-  manager: {},
-  engineer: [{}],
-  intern: [{}],
+  manager: [],
+  engineer: [],
+  intern: [],
   hasMemberToAdd: false
 };
 
@@ -20,15 +19,27 @@ const team = {
 // console.log(team.manager);
 async function init() {
   await addMemberToDB("manager", managerQuest);
-  await addMemberToDB("hasMemberToAdd", memberCheck);
+  //   await addMemberToDB("hasMemberToAdd", memberCheck);
 
   if (team.hasMemberToAdd) {
   }
 }
 
 const addMemberToDB = async (member, memberQuest) => {
-  team[member] = await askAbout(memberQuest);
-  console.log(team);
+  // Get answers
+  const data = await askAbout(memberQuest);
+
+  // Create class obj
+  if (member === "manager") {
+    const employeeObj = new Manager(data.name, data.id, data.email, data.officeNumber);
+  } else if (member === "engineer") {
+    const employeeObj = new Engineer(data.name, data.id, data.email, data.gitHubUsername);
+  } else {
+    const employeeObj = new Intern(data.name, data.id, data.email, data.school);
+  }
+
+  // Add it to DB
+  team[member].push(employeeObj);
 };
 
 init();
