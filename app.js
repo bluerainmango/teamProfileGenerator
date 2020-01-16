e606900d52994803d48937c45e098f85c4e5eeb9
Emily Yu <bluerainmango@gmail.com>
2020-01-15 18:55:14 -0800

    added inquiry.js

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 // prompt for team manager
 
 // prompt for team members - engineer / inern
-// name, email, id, engineer: github username, intern: school
+// name, email, id, manager: officeNumber engineer: github username, intern: school
+const {
+  askAbout,
+  managerQuest,
+  memberQuest,
+  memberCheck
+} = require("./lib/inquiry");
+
+const team = {
+  manager: {},
+  engineer: [{}],
+  intern: [{}],
+  hasMemberToAdd: false
+};
+
+// team.manager = askAbout(manager);
+// console.log(team.manager);
+async function init() {
+  await addMemberToDB("manager", managerQuest);
+  await addMemberToDB("hasMemberToAdd", memberCheck);
+
+  if (team.hasMemberToAdd) {
+  }
+}
+
+const addMemberToDB = async (member, memberQuest) => {
+  team[member] = await askAbout(memberQuest);
+  console.log(team);
+};
+
+init();
